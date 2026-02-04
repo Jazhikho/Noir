bfaa3c008e4d2fcfcb91d2741701efa67890f046
shiverrsin <bambino040304@gmail.com>
2026-02-04 02:35:01 -0600

    UI Work
    Added a menu/pause button in game. Revamped title screen a bit. Added button functionality throughout.

---
+++ Private Eye Pierce & The Ink Spill/Assets/Scripts/UIController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    void Start()
+    {
+        // Make sure pause menu starts hidden
+        if (pauseMenu != null)
+        {
+            pauseMenu.SetActive(false);
+        }
+    }
+
+    public void ShowPauseMenu()
+    {
+        pauseMenu.SetActive(true);
+    }
+
+    public void HidePauseMenu()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void TogglePauseMenu()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
